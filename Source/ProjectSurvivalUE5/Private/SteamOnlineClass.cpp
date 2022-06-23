688b260351116dfb7c65b6ef52faebddbe0e2ab8
Jefferson Douglas Sabino Miranda <89486965+CaptJackBlacK@users.noreply.github.com>
2022-06-23 18:24:15 -0300

    Multiplayer Configuração Padrão
    Configuração padrão de multiplayer do projeto

---
+++ Source/ProjectSurvivalUE5/Private/SteamOnlineClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SteamOnlineClass.h"
+
+SteamOnlineClass::SteamOnlineClass()
+{
+}
+
+SteamOnlineClass::~SteamOnlineClass()
+{
+}
